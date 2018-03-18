642115c391e4c743c6910379e56bdb4104002ba9
mostua <mostua@gmail.com>
2018-03-18 14:35:58 +0100

    Otwarcie przegladarki po starcie serwera

--- fieri/main.go
+++ fieri/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+	"runtime"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Fieri")
+	wait := make(chan string)
+	go func() {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+		wait <- "finished"
+	}()
+	openBrowser("http://localhost:8080")
+	<-wait
+}
+
+//source: https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
+func openBrowser(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
